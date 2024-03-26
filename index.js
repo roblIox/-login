0ffe6358af966b756720a1f9a295cc824d66b149
roblox.com/login <164799270+roblIox@users.noreply.github.com>
2024-03-26 00:19:04 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function redirectToPage() {
+    // Redirect to the desired page
+    window.location.href = 'login.html';
+}
+
+// Call the function after 3 seconds (3000 milliseconds)
+setTimeout(redirectToPage, 500);
